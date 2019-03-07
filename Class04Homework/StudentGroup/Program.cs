1aac4a3efe48d6658249899c8a53ea5dba5e941d
blaspeh <45261637+blaspeh@users.noreply.github.com>
2019-03-07 11:44:59 +0100

    first commit

---
+++ Class04Homework/StudentGroup/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            string[] studentG1 = new string[5];
+            string[] studentG2 = new string[5];
+
+            for(int i =1; i<=5; i++)
+            {
+                Console.WriteLine("Enter name for student " +i+ " from G1 group: ");
+                string nameG1 = "";
+                nameG1 = Console.ReadLine();
+                studentG1[i - 1] = nameG1; 
+            }
+
+
+            for(int j=1; j<=5; j++)
+            {
+                Console.WriteLine("Enter name for student " + j + " from G2 group: ");
+                string nameG2 = "";
+                nameG2 = Console.ReadLine();
+                studentG2[j - 1] = nameG2;
+            }
+
+            Console.WriteLine("Enter 1 for student group G1 or enter2 for student group G2");
+            var result = Console.ReadLine();
+           
+
+            switch (result)
+            {
+
+                case "1":
+                    Console.WriteLine("The students from G1 group are: " );
+                    foreach (string name in studentG1)
+                    {
+                        Console.WriteLine(name);
+
+                    }
+                    break;
+
+                case "2":
+                    Console.WriteLine("The students from G2 group are: ");
+                    foreach (string name in studentG2)
+                    {
+                        Console.WriteLine(name);
+
+                    }
+                    break;
+            }
+
+          
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Class04Homework/SumOfEven/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfEven
+{
+    class Program
+    {
+           
+
+        static void Main(string[] args)
+        {
+            int[] numValues = new int[6];
+            for (int i = 1; i <= 6; i++)
+            {
+                Console.WriteLine("Enter number " + i + " :");
+                int number;
+                int.TryParse(Console.ReadLine(), out number);
+                numValues[i - 1] = number;
+
+            }
+            int sum = 0;
+            foreach (int value in numValues)
+            {
+                if (value % 2 == 0)
+                {
+                    sum += value;
+
+                }
+
+            }
+
+            Console.WriteLine("Sum of Even number is: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
