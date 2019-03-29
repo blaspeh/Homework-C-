4b79f967922c3a0b0c52372e037f1d260f7511b4
blaspeh <45261637+blaspeh@users.noreply.github.com>
2019-03-29 13:43:24 +0100

    Homework Class02 C# Advance

---
+++ Class_02_Homework_CSharpAdv/InterfacesDemo/ClassesAndInterfaces/IAnimal.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDemo.ClassesAndInterfaces
+{
+    public interface IAnimal
+    {
+        string Name { get; set; }
+       
+    }
+
+    public interface IPredator : IAnimal
+    {
+        int AttackSpeed { get; set; }
+        string Attack(IPray pray);
+
+    }
+
+    public interface IPray : IAnimal
+    {
+        int FleeSpeed { get; set; }
+    }
+
+   
+     class Fish : IPredator, IPray
+    {
+       
+        public string Name { get; set; }
+        public int AttackSpeed { get; set; }
+        public int FleeSpeed { get; set; }
+
+
+        public string Attack(IPray pray)
+        {
+
+
+            return AttackSpeed > pray.FleeSpeed ? $"{Name} catches {pray.Name}" : $"{pray.Name} escapes from {Name}";
+        }
+
+
+        public Fish(string name, int fleeSpeed)
+        {
+            Name = name;
+            FleeSpeed = fleeSpeed;
+    
+        }
+
+        public Fish( int attackSpeed, string name)
+        {
+            Name = name;
+            AttackSpeed = attackSpeed;
+
+        }
+
+        public Fish()
+        {
+        }
+    }
+
+    //////////////////////////////////////////////////////////////////
+
+    class Cat : IPredator, IPray
+    {
+        public string Name { get; set; }
+        public int AttackSpeed { get; set; }
+        public int FleeSpeed { get; set; }
+
+        public string Attack(IPray pray)
+        {
+            return AttackSpeed > pray.FleeSpeed ? $"{Name} catches {pray.Name}" : $"{pray.Name} escapes from {Name}";
+        }
+
+        public Cat(string name, int fleeSpeed)
+        {
+            Name = name;
+            FleeSpeed = fleeSpeed;
+        }
+
+        public Cat(int attackSpeed, string name)
+        {
+            Name = name;
+            AttackSpeed = attackSpeed;
+        }
+
+        public Cat()
+        {
+
+        }
+
+    }
+
+    class Gazella : IPray
+    {
+        public string Name { get; set; }
+        public int FleeSpeed { get; set; }
+
+        public Gazella(string name, int fleeSpeed)
+        {
+            Name = name;
+            FleeSpeed = fleeSpeed;
+        }
+
+        public Gazella()
+        {
+            
+        }
+    }
+
+    
+
+}
---
+++ Class_02_Homework_CSharpAdv/InterfacesDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterfacesDemo.ClassesAndInterfaces;
+
+namespace InterfacesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IPredator shark = new Fish() { AttackSpeed = 30, Name = "Shark" };
+            IPray pinguin = new Fish() { FleeSpeed = 25, Name = "Pinguin" };
+            IPray herring = new Fish() { FleeSpeed = 35, Name = "Herring" };
+            IPray pastrmka = new Fish() { FleeSpeed = 50, Name = "Pastrmka" };
+            IPray pastrmka1 = new Fish() { FleeSpeed = 10, Name = "Pastrmka1" };
+
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine(shark.Attack(pinguin));
+            Console.WriteLine(shark.Attack(herring));
+            Console.WriteLine(shark.Attack(pastrmka));
+            Console.WriteLine(shark.Attack(pastrmka1));
+            Console.WriteLine("-------------------------------------");
+
+
+            IPredator gepard = new Cat() { AttackSpeed = 60, Name = "Gepard" };
+            IPray gazella = new Gazella() { FleeSpeed = 55, Name = "Gazella" };
+
+            Console.WriteLine(gepard.Attack(gazella));
+            Console.WriteLine("-------------------------------------");
+
+            IPredator lion = new Cat { AttackSpeed = 35, Name = "Lion" };
+            Console.WriteLine(lion.Attack(gazella));
+            Console.WriteLine("-------------------------------------");
+
+            Console.ReadLine();
+
+        }
+    }
+}
